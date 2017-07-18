4d1cd5908f9a43d70385056451c86f575dd1a342
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-07-18 14:01:43 -0500

    completed step 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+//Components
+import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+
+
+//App View
 class App extends Component {
   render() {
-    
+    return (
+      <TopicBrowser/>
+    )
+
   }
 }
 
